989f249016dbd7ee91617d4ce53e9ea463417845
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2025-11-27 17:08:49 +0330

    Word pattern

---
+++ pracitce_part3/word_pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def word_pattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(words) != len(pattern):
+            return False
+        char_to_word = {}
+        word_to_char = {}
+        for c, w in zip(pattern, words):
+            if c not in char_to_word:
+                char_to_word[c] = w
+            if w not in word_to_char:
+                word_to_char[w] = c
+            if char_to_word[c] != w or word_to_char[w] != c:
+                return False
+        return True
